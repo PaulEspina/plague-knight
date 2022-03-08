82250e7f04e049d7908a4f185a793cc0eddf7898
Paul Espina <kristopherespina@gmail.com>
2022-03-08 13:52:08 +0800

    Update entity class

--- src/main/entity/Entity.java
+++ src/main/entity/Entity.java
@@ -1,17 +1,86 @@
 package main.entity;
 
+import main.gfx.ImageLoader;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public abstract class Entity
 {
-    private Point pos;
-    private Point size;
-    String texture;
-    int animationSpeed;
-    int movementSpeed;
+    protected Point pos;
+    protected Point size;
+    protected BufferedImage texture;
+    protected int animationSpeed;
+    protected int movementSpeed;
 
     public void move(Point vel)
     {
         pos.translate(vel.x, vel.y);
+    }
+
+    public Point getPos()
+    {
+        return pos;
+    }
+
+    public void setPos(Point pos)
+    {
+        this.pos = pos;
+    }
+
+    public Point getSize()
+    {
+        return size;
+    }
+
+    public void setSize(Point size)
+    {
+        if(size.x < 0)
+        {
+            size.x = 0;
+        }
+        if(size.y < 0)
+        {
+            size.y = 0;
+        }
+        this.size = size;
+    }
+
+    public BufferedImage getTexture()
+    {
+        return texture;
+    }
+
+    public void setTexture(String path)
+    {
+        texture = ImageLoader.loadImage(path);
+    }
+
+    public int getAnimationSpeed()
+    {
+        return animationSpeed;
+    }
+
+    public void setAnimationSpeed(int animationSpeed)
+    {
+        if(animationSpeed <= 0)
+        {
+            animationSpeed = 1;
+        }
+        this.animationSpeed = animationSpeed;
+    }
+
+    public int getMovementSpeed()
+    {
+        return movementSpeed;
+    }
+
+    public void setMovementSpeed(int movementSpeed)
+    {
+        if(movementSpeed < 0)
+        {
+            movementSpeed = 0;
+        }
+        this.movementSpeed = movementSpeed;
     }
 }
