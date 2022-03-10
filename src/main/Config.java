e307208b5031f1fdbd303ff144d3fbae4967efd7
Paul Espina <kristopherespina@gmail.com>
2022-03-10 15:23:02 +0800

    Zombie texture

--- src/main/Config.java
+++ src/main/Config.java
@@ -5,4 +5,13 @@ public class Config
     public static final int SCREEN_WIDTH = 800;
     public static final int SCREEN_HEIGHT = 600;
     public static final String TITLE = "Plague Knight";
+
+    public static final String ZOMBIE_SPRITE_PATH = "/assets/sprites/zombies/zombiesmainv1.png";
+    public static final int ZOMBIE_SPRITE_WIDTH = 36;
+    public static final int ZOMBIE_SPRITE_HEIGHT = 48;
+
+    public static final float DEGREES_NORTH = -90f;
+    public static final float DEGREES_SOUTH = 90f;
+    public static final float DEGREES_EAST = 0f;
+    public static final float DEGREES_WEST = 180f;
 }
