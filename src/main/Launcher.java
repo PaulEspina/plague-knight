12b3fddd48e99244be43b83aa4f85fbf3f98bb32
Paul Espina <kristopherespina@gmail.com>
2022-03-17 13:16:59 +0800

    JOptionPane for errors and Manifest shits

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -2,16 +2,26 @@ package main;
 
 import main.gfx.AssetManager;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
 public class Launcher
 {
     public static void main(String[] args)
     {
         System.out.println("Loading assets...");
-        if(AssetManager.getInstance().load())
+        try
         {
-            System.out.println("Assets loaded.");
-            Game game = new Game();
-            game.start();
+            AssetManager.getInstance().load();
         }
+        catch(IOException | FontFormatException e)
+        {
+            JOptionPane.showMessageDialog(null, "Failed to load assets.", "Failed to Launch.", JOptionPane.ERROR_MESSAGE);
+            System.exit(-1);
+        }
+        System.out.println("Assets loaded.");
+        Game game = new Game();
+        game.start();
     }
 }
--- src/main/gfx/AssetManager.java
+++ src/main/gfx/AssetManager.java
@@ -5,6 +5,7 @@ import main.Config;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -56,51 +57,41 @@ public class AssetManager
         return instance;
     }
 
-    public boolean load()
+    public void load() throws IOException, FontFormatException
     {
-        try
-        {
-            map = ImageLoader.loadImage(Config.MAP_PATH);
-            player = ImageLoader.loadImage(Config.PLAYER_SPRITE_PATH);
-            zombie = ImageLoader.loadImage(Config.ZOMBIE_ASSET_PATH);
-            crate = ImageLoader.loadImage(Config.CRATE_ASSET_PATH);
-            HUD = ImageLoader.loadImage(Config.HUD_ASSET_PATH);
-            item = ImageLoader.loadImage(Config.ITEMS_ASSET_PATH);
-            menuButtonImage = ImageLoader.loadImage(Config.MENU_BUTTON_ASSET_PATH);
-            difficultyImage = ImageLoader.loadImage(Config.DIFFICULTY_ASSET_PATH);
-            defaultBGImage = ImageLoader.loadImage(Config.MENU_BACKGROUND_ASSET_PATH);
-            dottedBGImage = ImageLoader.loadImage(Config.DOTTED_BACKGROUND_ASSET_PATH);
-            exitBGImage = ImageLoader.loadImage(Config.EXIT_MENU_ASSET_PATH);
-            survivalBrokenBGImage = ImageLoader.loadImage(Config.BROKEN_SURVIVAL_BACKGROUND_ASSET_PATH);
-            storyBrokenBGImage = ImageLoader.loadImage(Config.BROKEN_STORY_BACKGROUND_ASSET_PATH);
-            pausedImage = ImageLoader.loadImage(Config.PAUSED_TEXT_ASSET_PATH);
-            youDiedImage = ImageLoader.loadImage(Config.YOU_DIED_ASSET_PATH);
-            youDiedBG = ImageLoader.loadImage(Config.DIED_BG_PATH);
-            youPausedBG = ImageLoader.loadImage(Config.PAUSE_BG_PATH);
+        map = ImageLoader.loadImage(Config.MAP_PATH);
+        player = ImageLoader.loadImage(Config.PLAYER_SPRITE_PATH);
+        zombie = ImageLoader.loadImage(Config.ZOMBIE_ASSET_PATH);
+        crate = ImageLoader.loadImage(Config.CRATE_ASSET_PATH);
+        HUD = ImageLoader.loadImage(Config.HUD_ASSET_PATH);
+        item = ImageLoader.loadImage(Config.ITEMS_ASSET_PATH);
+        menuButtonImage = ImageLoader.loadImage(Config.MENU_BUTTON_ASSET_PATH);
+        difficultyImage = ImageLoader.loadImage(Config.DIFFICULTY_ASSET_PATH);
+        defaultBGImage = ImageLoader.loadImage(Config.MENU_BACKGROUND_ASSET_PATH);
+        dottedBGImage = ImageLoader.loadImage(Config.DOTTED_BACKGROUND_ASSET_PATH);
+        exitBGImage = ImageLoader.loadImage(Config.EXIT_MENU_ASSET_PATH);
+        survivalBrokenBGImage = ImageLoader.loadImage(Config.BROKEN_SURVIVAL_BACKGROUND_ASSET_PATH);
+        storyBrokenBGImage = ImageLoader.loadImage(Config.BROKEN_STORY_BACKGROUND_ASSET_PATH);
+        pausedImage = ImageLoader.loadImage(Config.PAUSED_TEXT_ASSET_PATH);
+        youDiedImage = ImageLoader.loadImage(Config.YOU_DIED_ASSET_PATH);
+        youDiedBG = ImageLoader.loadImage(Config.DIED_BG_PATH);
+        youPausedBG = ImageLoader.loadImage(Config.PAUSE_BG_PATH);
 
-            menuBGM = AudioLoader.loadAudio(Config.MAIN_MENU_BG_MUSIC_PATH);
-            gameBGM = AudioLoader.loadAudio(Config.IN_GAME_MUSIC_PATH);
-            knifeFX = AudioLoader.loadAudio(Config.KNIFE_STAB_MUSIC_PATH);
-            enterPressFX = AudioLoader.loadAudio(Config.ENTER_GAME_PATH);
-            buttonPressFX = AudioLoader.loadAudio(Config.BUTTON_PRESS_PATH);
-            zombie1FX = AudioLoader.loadAudio(Config.ZOMBIE_1_MUSIC_PATH);
-            zombie2FX = AudioLoader.loadAudio(Config.ZOMBIE_2_MUSIC_PATH);
-            zombie3FX = AudioLoader.loadAudio(Config.ZOMBIE_3_MUSIC_PATH);
+        menuBGM = AudioLoader.loadAudio(Config.MAIN_MENU_BG_MUSIC_PATH);
+        gameBGM = AudioLoader.loadAudio(Config.IN_GAME_MUSIC_PATH);
+        knifeFX = AudioLoader.loadAudio(Config.KNIFE_STAB_MUSIC_PATH);
+        enterPressFX = AudioLoader.loadAudio(Config.ENTER_GAME_PATH);
+        buttonPressFX = AudioLoader.loadAudio(Config.BUTTON_PRESS_PATH);
+        zombie1FX = AudioLoader.loadAudio(Config.ZOMBIE_1_MUSIC_PATH);
+        zombie2FX = AudioLoader.loadAudio(Config.ZOMBIE_2_MUSIC_PATH);
+        zombie3FX = AudioLoader.loadAudio(Config.ZOMBIE_3_MUSIC_PATH);
 
-            arcadeClassicSmall = Font.createFont(Font.TRUETYPE_FONT, new File("ARCADECLASSIC.TTF")).deriveFont(30f);
+        arcadeClassicSmall = Font.createFont(Font.TRUETYPE_FONT, new File("ARCADECLASSIC.TTF")).deriveFont(30f);
 
 
-            arcadeClassic = Font.createFont(Font.TRUETYPE_FONT, new File("ARCADECLASSIC.TTF")).deriveFont(72f);
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("ARCADECLASSIC.TTF")));
-//            treeObstacle = ImageLoader.loadImage(Config.TREE_OBSTACLE_ASSET_PATH);
-        }
-        catch(IllegalArgumentException | IOException | FontFormatException e)
-        {
-            System.err.println("Failed to load assets.");
-            return false;
-        }
-        return true;
+        arcadeClassic = Font.createFont(Font.TRUETYPE_FONT, new File("ARCADECLASSIC.TTF")).deriveFont(72f);
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("ARCADECLASSIC.TTF")));
     }
 
     public BufferedImage getPlayer()
